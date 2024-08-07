5c662d2c879652803a6345b199e39f62686033b6
Krishna-Nayak603 <krishna.nayak@godigit.com>
2024-08-07 16:59:14 +0530

    Merge: still not fully null also get stored

---
+++ src/Problem_MergeMultipleFiles/Main.java
@@ -0,0 +1,38 @@
+package Problem_MergeMultipleFiles;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+    public static void main(String[] args) {
+        Path dirPath = Paths.get("src/TextFile");
+        Path source_path = Paths.get("src/Problem_MergeMultipleFiles/final_merge.txt");
+        try (OutputStream outputStream = Files.newOutputStream(source_path, StandardOpenOption.CREATE)) {
+            Files.list(dirPath).forEach(filePathName -> {
+                try (FileInputStream inputStream = new FileInputStream(String.valueOf(filePathName))) {
+
+                    FileChannel fileChannel = inputStream.getChannel();
+                    ByteBuffer buff = ByteBuffer.allocate(100);
+
+                    fileChannel.read(buff);
+
+                    outputStream.write(buff.array());
+
+                    System.out.println("File Merge:\t" + filePathName);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
